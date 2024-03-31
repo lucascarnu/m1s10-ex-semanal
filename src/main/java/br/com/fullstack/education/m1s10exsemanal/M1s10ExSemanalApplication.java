7a661cc83f34a26eb49d90ab7230fd58962b2b80
lucascarnu <lucascarnu@gmail.com>
2024-03-31 20:35:33 -0300

    m1s10_ex_semanal_db_setup

---
+++ src/main/java/br/com/fullstack/education/m1s10exsemanal/M1s10ExSemanalApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.m1s10exsemanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1s10ExSemanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M1s10ExSemanalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fullstack/education/m1s10exsemanal/M1s10ExSemanalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.m1s10exsemanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1s10ExSemanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
